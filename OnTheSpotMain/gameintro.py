59a8bcaaef4abd34150e0dc11dbfc99b8050de6d
Dnewman9 <69917551+Dnewman9@users.noreply.github.com>
2020-11-20 21:01:27 -0500

    Renamed for clarification

--- OnTheSpotMain/gameintro.py
+++ OnTheSpotMain/gameintro.py
@@ -1,7 +1,7 @@
 print('Welcome to On The Spot! Please have fun/')
 # begining the game. asking if wanting to start then importing questions
 
-response = input("Press "R" if ready to take the quiz or "Q" to quiz\n")
+response = input("Press "R" if ready to take the quiz or 'Q' to quiz\n")
 
 if response.lower() == 'R':
     import pytrivia 
