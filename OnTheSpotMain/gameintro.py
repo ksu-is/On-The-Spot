3a0604b03b61b561c4cd4e95976ea73663222394
Dnewman9 <69917551+Dnewman9@users.noreply.github.com>
2020-11-20 21:12:35 -0500

    Update gameintro.py
    commenting and Todo for game introduction

--- OnTheSpotMain/gameintro.py
+++ OnTheSpotMain/gameintro.py
@@ -1,11 +1,12 @@
 print('Welcome to On The Spot! Please have fun/')
 # begining the game. asking if wanting to start then importing questions
 
-response = input("Press "R" if ready to take the quiz or 'Q' to quiz\n")
+response = input("Press 'R' if ready to take the quiz or 'Q' to quiz\n")
 
 if response.lower() == 'R':
     import pytrivia 
 elif response.lower() == 'Q':
     return "Thank you for playing"
 else:
-    print "You did not choose a valid option"
+    print("You did not choose a valid option")
+    
