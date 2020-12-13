503384f5c2df3668849bc242a63fe4bd02fa45f3
Dnewman9 <69917551+Dnewman9@users.noreply.github.com>
2020-12-12 22:18:35 -0500

    Main Function Implementations

--- OnTheSpotMain/OTS.py
+++ OnTheSpotMain/OTS.py
@@ -43,60 +43,94 @@ with urllib.request.urlopen(TopicURL) as url:
     data = json.loads(url.read().decode())
     cat = data["trivia_categories"]
 
+loopMenu = True
 
+while loopMenu:
+    players = input("How many players? ")
+    if players == "":
+        players = "1"
+    if (players.isalpha() == True or int(players) < 0):
+        print("Not valid answer")
+    else:
+        players = int(players)
+        print("0. All")
+        for i in range(1, len(cat) + 1):
+            print(str(i) + ". " + cat[i-1]["name"])
 
-def playerSetup():
-
-    """  
-    Welcome the player, and ask for names to initialize player profiles
-    """
-    print("\n       ------ !! Let's Get Started !! ------\n")
-    
-    while True:
-        player_count = input("How many players will be participating? (2-4): ")
-        if player_count.isalpha():
-            print("Invalid Input.\n")
-        else:
-            player_count = int(player_count)
-            if player_count < 2:
-                print("Minimum 2 players required.\n")
-            elif player_count > 4:
-                print("Maximum 4 players allowed.\n")
+        TOPIC = ""
+        DIFFICULTY = ""
+        loop = True
+        while loop:
+            sel = input("Choose category: ")
+            if sel == "":
+                sel = "0"
+            if (sel.isalpha() == True or int(sel) not in range(len(cat) + 1)):
+                print("Not valid answer")
             else:
-                break
+                if (sel != "0" and sel != ""):
+                    TOPIC = "&category=" + str(cat[int(sel) - 1]["id"])
+                loop = False
 
-    for i in range(0, player_count):
-        name = input("Player " + str(i+1) + ", Enter your name: ")
-        name = name.title()
-        players["player"+str(1+i)] = {'name': name, 'points': 0, 'streak': 0}
-    return player_count, players
+        print("1. Easy\n2. Medium\n3. Hard\n4. All")
+        loop = True
+        while loop:
+            sel = input("Choose difficulty: ")
+            if sel == "":
+                sel = "0"
+            if (sel.isalpha() == True or int(sel) not in range(5)):
+                print("Not valid answer")
+            else:
+                if (sel == "1"):
+                    DIFFICULTY = "&difficulty=easy"
+                elif (sel == "2"):
+                    DIFFICULTY = "&difficulty=medium"
+                elif (sel == "3"):
+                    DIFFICULTY = "&difficulty=hard"
+                loop = False
 
-def gameRules():
-    print("\n       ------ !! Welcome to On The Spot !! ------\n")
-    print('The rules are simple. There are 9 rounds of trivia.\n')
-    print('Each round will have 3 randomized catogories to choose from. Players will each choose their difficulty and answer the question.\n')
-    print('For every 2 questions in a row correct, players will be given the opportunity for an "On The Spot" question.\n')
-    print('The player with the most points wins.')
-    
-    player_status = ""
-    while True:
-        player_status = input("\nAre you ready to begin? (y/n): ")
-        if player_status.lower() == 'y':
-            print("Let's begin!")
-            return
-        elif player_status.lower() == 'n':
-            print('Exiting game. Thank you.')
-            exit()
-        else:
-            print("Invalid Input.")
+        questions = []
+        points = []
 
+        for pl in range(players):
+            questions.append(generate_questions(n, TOKEN, TOPIC, DIFFICULTY))
+            points.append(0)
 
-def game_control():
-    '''
-    Control the whole game with the single steps.
-    '''
-    gameReset()
-    playerSetup()
-    gameRules()
+        for i in range(n):
+            for pl in range(players):
+                if players == 1:
+                    x = input("It's your turn")
+                else:
+                    x = input("Player " + str(pl + 1) + ", it's your turn")
+                loop = True
+                quest = questions[pl]["results"][i]
+                QuestionType = quest["type"]
+                print("Question number " + str(i + 1))
+                if (TOPIC == ""):
+                    print("Category: " + quest["category"])
+                if (DIFFICULTY == ""):
+                    print("Difficulty: " + quest["difficulty"])
+                print(html.unescape(quest["question"]))
 
-game_control()
+                answers = generate_answers(quest, QuestionType)
+                for k in range(len(answers)):
+                    print(str(k + 1) + ". " + html.unescape(answers[k]))
+                while (loop):
+                    answer = input("Choice: ")
+                    if (answer == ""):
+                        answer = "0"
+                    if (answer.isalpha() == True or int(answer) not in range(len(answers) + 1)):
+                        print("Not valid answer")
+                    elif (answers[int(answer) - 1] == quest["correct_answer"]):
+                        print("Correct!")
+                        points[pl] += 1
+                        loop = False
+                    else:
+                        if (answer != "0"):
+                            print("Wrong!")
+                        print("The correct answer is " +
+                              html.unescape(quest["correct_answer"]))
+                        loop = False
+
+        for pl in range(players):
+            print("Points player " + str(pl + 1) + ": " + str(points[pl]))
+        loopMenu = False
